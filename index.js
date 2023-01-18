d8c77403529e81c0fa49403e60dc4e24b8946cba
catmajor <119273404+catmajor@users.noreply.github.com>
2023-01-18 10:02:49 -0500

    more code

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 /*
 TODO
-Draw Spider Web:
- find point at certain radius
- draw line connecting two points
 Spider onLine() function (returning the line that the spider is on)
 Spider rotationMatches() function (test that the line the spider is on is within a couple degrees)
 Spider move(int) function move forward or backward
  
 */
- 
-const center = document.querySelector("center")
+
 const sprite = document.querySelector("sprite")
-const edgeConnectors = document.querySelectorAll('toEdges *')
+const toEdge = document.querySelector('toEdges')
 const connectEC = document.querySelector("edgeConnect")
+let allLines = []
 let windowDimensions = {x: window.innerWidth, y: window.innerHeight}
  
  
-const topL = edgeConnectors[0]
-const topR = edgeConnectors[1]
-const bottomL = edgeConnectors[2]
-const bottomR = edgeConnectors[3]
  
 const randomX = Math.random()/2
 const randomY = Math.random()/2
+ 
+class Web {
+  constructor() {
+    
+  }
+}
  
 class Point {
  constructor(x = null, y = null) {
@@ -81,10 +80,12 @@ class Line {
      this.origin = {x:point.x-this.x, y:point.y-this.y}
    }
    else throw new Error("NotEnoughInfo")
+   this.collisionBox = this.domElement.getBoundingClientRect()
+   allLines.push(this)
  }
   render() {
    this.domElement.style.left = `${this.origin.x}px`
-   this.domElement.style.top = `${this.origin.y}px`
+   this.domElement.style.top = `${this.origin.y-5}px`
    this.domElement.style.width = this.length
    this.domElement.style.transform = `rotate(${this.theta}rad)`
  }
@@ -99,6 +100,7 @@ class Line {
   let cosTheta = Math.acos(this.cos)
   this.theta = this.sin<0?-cosTheta:cosTheta
   this.render()
+  this.collisionBox = this.domElement.getBoundingClientRect()
  }
  updatePoint(coord) {
   this.point = coord
@@ -111,6 +113,7 @@ class Line {
   let cosTheta = Math.acos(this.cos)
   this.theta = this.sin<0?-cosTheta:cosTheta
   this.render()
+  this.collisionBox = this.domElement.getBoundingClientRect()
  }
  
  circlePoint(radius) {
@@ -120,13 +123,21 @@ class Line {
  }
 }
 
+class CircleLine extends Line {
+  constructor(domParent = false, domElement = false, origin = false, point = false, theta = false, length = false) {
+    super(domParent, domElement, origin, point, theta, length)
+    this.b = origin.y-origin.x*this.tan
+  }
+}
+
+
 class Circle {
   constructor(radius) {
     this.radius = radius
     let connectionLines = []
     edgeLines.forEach((ele, ind) => {
       let secondLine = ind===edgeLines.length-1?0:ind+1
-      let temp = new Line(connectEC, false, edgeLines[ind].circlePoint(radius), edgeLines[secondLine].circlePoint(radius))
+      let temp = new CircleLine(connectEC, false, edgeLines[ind].circlePoint(radius), edgeLines[secondLine].circlePoint(radius))
       temp.render()
       connectionLines.push(temp)
     })
@@ -142,38 +153,55 @@ class Circle {
  }
 }
  
+class Spider {
+  constructor(domElement, position) {
+    this.position = position
+    this.domElement = domElement
+    this.theta = 0
+  }
+  render() {
+   this.domElement.style.left = `${this.position.x}px`
+   this.domElement.style.top = `${this.position.y-5}px`
+   this.domElement.style.transform = `rotate(${this.theta}rad)`
+}
+ 
+ 
 let defaultCenter = {x: (windowDimensions.x/4 + windowDimensions.x*randomX), y: (windowDimensions.y/4 + windowDimensions.y*randomY)}
  
 
 let right = windowDimensions.x
 let bottom = windowDimensions.y
-let halfright = right/2
+let thirdright = right/3
 let halfbottom = bottom/2
 let edgeLines = []
 
-const corners = [
+const edgeConnections = [
   {x: right, y: bottom},
-  {x: right, y: halfbottom}, 
-  {x: right, y: 0}, 
-  {x: halfright, y: 0},
-  {x: 0, y: 0},   
-  {x: 0, y: halfbottom},  
-  {x: 0, y: bottom},  
-  {x: halfright, y: bottom}
+  {x: right, y: halfbottom},
+  {x: right, y: 0},
+  {x: thirdright*2, y: 0},
+  {x: thirdright, y: 0},
+  {x: 0, y: 0},
+  {x: 0, y: halfbottom},
+  {x: 0, y: bottom},
+  {x: thirdright, y: bottom},
+  {x: thirdright*2, y: bottom}
 ]
-edgeConnectors.forEach((ele, ind) => {
-  temp = new Line(null, ele, defaultCenter, corners[ind])
+edgeConnections.forEach((ele, ind) => {
+  temp = new Line(toEdge, null, defaultCenter, ele)
   temp.render()
   edgeLines.push(temp)
 })
 const screenLength = Math.sqrt(Math.pow(right, 2) + Math.pow(bottom, 2))
 const halfScreen = screenLength/2
-const numberOfCircles = Math.floor(Math.random()*4+1)
-const allCircles = new Array(numberOfCircles).fill(1).map((ele, ind) => {
-  let temp = new Circle(halfScreen*ind/(numberOfCircles+1))
-  return temp
-})
-//[new Circle(screenLength/8), new Circle(screenLength/4), new Circle(screenLength/8*3)]
+const numberOfCircles = Math.floor(Math.random()*4+2)
+let allCircles = []
+for (let i=1; i<numberOfCircles+1; i++) {
+  let temp = new Circle(halfScreen*i/(numberOfCircles+1))
+  allCircles.push(temp)
+}
+let test = new Spider(sprite, defaultCenter)
+test.render()
 
 
 
@@ -182,12 +210,7 @@ const allCircles = new Array(numberOfCircles).fill(1).map((ele, ind) => {
 
 
 function renderWeb(centerCoords = defaultCenter) {
- let right = windowDimensions.x
- let bottom = windowDimensions.y
- let halfright = right/2
- let halfbottom = bottom/2
- center.style.left= `${centerCoords.x}px`
- center.style.top = `${centerCoords.y}px`
+ 
  
  edgeLines.forEach((ele, ind) => {
    ele.updateCenter(centerCoords)
@@ -196,25 +219,25 @@ function renderWeb(centerCoords = defaultCenter) {
 allCircles.forEach(ele => ele.updateCircle())
 }
  
- 
- 
- 
- 
- 
- 
- 
- 
 window.addEventListener('resize', event => {
  windowDimensions = {x: window.innerWidth, y: window.innerHeight}
  centerCoords = {x: (windowDimensions.x/4 + windowDimensions.x*randomX), y: (windowDimensions.y/4 + windowDimensions.y*randomY)}
  renderWeb()
 })
  
-const followMouse = true
-if (followMouse) {
- window.addEventListener('mousemove', event => {
+let followMouse = false
+
+window.addEventListener('mousemove', event => {
+  if (followMouse) {
      centerCoords = {x: event.clientX, y: event.clientY}
      renderWeb(centerCoords)
- })
-}
+  }
+})
+ sprite.textContent = allLines.length
+
  
+window.addEventListener('mousedown', event => {
+  followMouse = !followMouse
+  centerCoords = {x: event.clientX, y: event.clientY}
+  renderWeb(centerCoords)
+})
